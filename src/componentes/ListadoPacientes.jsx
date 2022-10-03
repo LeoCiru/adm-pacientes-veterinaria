464f08675453046b320c70f5b2cae948827b7648
Leo Vásquez Vólquez <vasquezvolquezfelixleonel@gmail.com>
2022-10-02 20:40:50 -0400

    Agregando texto condicional en el listado de pacientes

--- src/componentes/ListadoPacientes.jsx
+++ src/componentes/ListadoPacientes.jsx
@@ -2,20 +2,34 @@ import Paciente from "./Paciente"
 
 function ListadoPacientes({pacientes}) {
   return (
-    <section className="listado-pacientes">
-        <h2>No hay pacientes</h2>
-        <p>Comienza agregando pacientes y {''}
-            <span>aparecerán en este lugar</span>
-        </p>
+    <>
+      {
+        pacientes.length === 0 ? (
+          <section className="listado-pacientes">
+            <h2>No hay pacientes</h2>
+            <p>Comienza agregando pacientes y {''}
+                <span>aparecerán en este lugar</span>
+            </p>
+          </section>
+        ) : (
+          <section className="listado-pacientes">
+              <h2>Listado Pacientes</h2>
+              <p>Administra tus {''}
+                  <span>Pacientes y Citas</span>
+              </p>
+      
+              { pacientes.map( paciente => (
+                  <Paciente
+                  key={paciente.id}
+                    paciente={paciente}
+                  />
+              ))}
+          </section>
+        )
+      }
+    </>
 
-        { pacientes.map( paciente => (
-            <Paciente
-            key={paciente.id}
-              paciente={paciente}
-            />
-        ))}
 
-    </section>
   )
 }
 
