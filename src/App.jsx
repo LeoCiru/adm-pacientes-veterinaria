d2a577d3718c3505f94ad4745b4df095fcd99c6c
Leo Vásquez Vólquez <vasquezvolquezfelixleonel@gmail.com>
2024-02-29 23:23:49 -0400

    Agregando los registros al LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./componentes/Header";
 import FormularioPacientes from "./componentes/FormularioPacientes";
 import ListadoPacientes from "./componentes/ListadoPacientes";
 
 
 function App() {
-  const [pacientes, setPacientes] = useState([]);
+  const [pacientes, setPacientes] = useState(
+    localStorage.getItem('pacientes') ? JSON.parse(localStorage.getItem('pacientes')) : []
+  );
   const [paciente, setPaciente] = useState({});
+
+    
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes]);
+  
 
   function eliminarPaciente(id) {
     const pacientesActivos = pacientes.filter(pacienteActivo => pacienteActivo.id != id);
